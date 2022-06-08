5c5875d1eb6393b431c4f388f0ffe1bccf2ad70a
Amanyadav23 <88925295+Amanyadav23@users.noreply.github.com>
2022-06-08 22:22:13 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep =f"🎧 **ƲƤԼƠƛƊЄƦ : @A_4_AMAN_0fficial**"
+        rep =f"🎧 **ƲƤԼƠƛƊЄƦ : @OFFICIAL_MENTALMOD**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
